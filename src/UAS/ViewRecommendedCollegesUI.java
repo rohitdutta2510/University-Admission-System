8de74a6c5a62f70e6e317c845de1e13426cdf60b
samir2901 <thunder.cse2018@gmail.com>
2020-12-18 11:11:33 +0530

    Updated ViewRecommended Colleges

--- src/UAS/ViewRecommendedCollegesUI.java
+++ src/UAS/ViewRecommendedCollegesUI.java
@@ -29,15 +29,14 @@ public class ViewRecommendedCollegesUI extends javax.swing.JFrame {
         jCheckBox1 = new javax.swing.JCheckBox();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        collegeFilter = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
-        jComboBox2 = new javax.swing.JComboBox<>();
-        jCheckBox2 = new javax.swing.JCheckBox();
-        jCheckBox3 = new javax.swing.JCheckBox();
-        jCheckBox4 = new javax.swing.JCheckBox();
-        jCheckBox5 = new javax.swing.JCheckBox();
-        jCheckBox6 = new javax.swing.JCheckBox();
-        jCheckBox7 = new javax.swing.JCheckBox();
+        streamFilter = new javax.swing.JComboBox<>();
+        recommendCollege1 = new javax.swing.JCheckBox();
+        recommendCollege2 = new javax.swing.JCheckBox();
+        recommendCollege3 = new javax.swing.JCheckBox();
+        recommendCollege4 = new javax.swing.JCheckBox();
+        recommendCollege5 = new javax.swing.JCheckBox();
         jButton1 = new javax.swing.JButton();
 
         jCheckBox1.setText("jCheckBox1");
@@ -46,36 +45,34 @@ public class ViewRecommendedCollegesUI extends javax.swing.JFrame {
 
         jLabel1.setText("College");
 
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel2.setText("View Recommended Colleges");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        collegeFilter.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         jLabel3.setText("Stream");
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        streamFilter.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
-        jCheckBox2.setText("ABC College - Physics");
-        jCheckBox2.addActionListener(new java.awt.event.ActionListener() {
+        recommendCollege1.setText("ABC College - Physics");
+        recommendCollege1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jCheckBox2ActionPerformed(evt);
+                recommendCollege1ActionPerformed(evt);
             }
         });
 
-        jCheckBox3.setText("XYZ College - Physics");
+        recommendCollege2.setText("XYZ College - Physics");
 
-        jCheckBox4.setText("EFG College - Physics");
+        recommendCollege3.setText("EFG College - Physics");
 
-        jCheckBox5.setText("ABC College - Chemistry");
-        jCheckBox5.addActionListener(new java.awt.event.ActionListener() {
+        recommendCollege4.setText("ABC College - Chemistry");
+        recommendCollege4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jCheckBox5ActionPerformed(evt);
+                recommendCollege4ActionPerformed(evt);
             }
         });
 
-        jCheckBox6.setText("XYZ College - Chemistry");
-        jCheckBox6.setActionCommand("XYZ College - Chemistry");
-
-        jCheckBox7.setText("XYZ College - Chemistry");
+        recommendCollege5.setText("XYZ College - Chemistry");
 
         jButton1.setBackground(new java.awt.Color(236, 45, 25));
         jButton1.setText("Submit");
@@ -90,38 +87,37 @@ public class ViewRecommendedCollegesUI extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(layout.createSequentialGroup()
-                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
-                            .add(layout.createSequentialGroup()
-                                .add(179, 179, 179)
-                                .add(jLabel2))
-                            .add(layout.createSequentialGroup()
-                                .add(36, 36, 36)
-                                .add(jLabel1)
-                                .add(34, 34, 34)
-                                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                    .add(jCheckBox7)
-                                    .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                        .add(jCheckBox6)
-                                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                            .add(jCheckBox5)
-                                            .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                                .add(jCheckBox4)
-                                                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                                    .add(jCheckBox3)
-                                                    .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                                                        .add(jCheckBox2)
-                                                        .add(layout.createSequentialGroup()
-                                                            .add(jComboBox1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                                                            .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                                            .add(jLabel3))))))))))
-                        .add(30, 30, 30)
-                        .add(jComboBox2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                    .add(layout.createSequentialGroup()
-                        .add(262, 262, 262)
-                        .add(jButton1)))
+                .add(36, 36, 36)
+                .add(jLabel1)
+                .add(34, 34, 34)
+                .add(collegeFilter, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(212, 212, 212)
+                .add(jLabel3)
+                .add(30, 30, 30)
+                .add(streamFilter, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(99, Short.MAX_VALUE))
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                        .add(jLabel2)
+                        .add(177, 177, 177))
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                            .add(recommendCollege1)
+                            .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                                .add(recommendCollege3)
+                                .add(recommendCollege2)))
+                        .add(259, 259, 259))
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                        .add(jButton1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 91, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                        .add(275, 275, 275))
+                    .add(layout.createSequentialGroup()
+                        .add(30, 30, 30)
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                            .add(recommendCollege5)
+                            .add(recommendCollege4))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
@@ -131,36 +127,34 @@ public class ViewRecommendedCollegesUI extends javax.swing.JFrame {
                 .add(18, 18, 18)
                 .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
                     .add(jLabel1)
-                    .add(jComboBox1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(collegeFilter, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                     .add(jLabel3)
-                    .add(jComboBox2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .add(43, 43, 43)
-                .add(jCheckBox2)
+                    .add(streamFilter, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .add(18, 18, 18)
-                .add(jCheckBox3)
+                .add(recommendCollege1)
                 .add(18, 18, 18)
-                .add(jCheckBox4)
+                .add(recommendCollege2)
                 .add(18, 18, 18)
-                .add(jCheckBox5)
+                .add(recommendCollege3)
                 .add(18, 18, 18)
-                .add(jCheckBox6)
+                .add(recommendCollege4)
                 .add(18, 18, 18)
-                .add(jCheckBox7)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 16, Short.MAX_VALUE)
-                .add(jButton1)
-                .addContainerGap())
+                .add(recommendCollege5)
+                .add(60, 60, 60)
+                .add(jButton1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 37, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(28, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jCheckBox2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox2ActionPerformed
+    private void recommendCollege1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_recommendCollege1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jCheckBox2ActionPerformed
+    }//GEN-LAST:event_recommendCollege1ActionPerformed
 
-    private void jCheckBox5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBox5ActionPerformed
+    private void recommendCollege4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_recommendCollege4ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jCheckBox5ActionPerformed
+    }//GEN-LAST:event_recommendCollege4ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
@@ -202,19 +196,18 @@ public class ViewRecommendedCollegesUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> collegeFilter;
     private javax.swing.JButton jButton1;
     private javax.swing.JCheckBox jCheckBox1;
-    private javax.swing.JCheckBox jCheckBox2;
-    private javax.swing.JCheckBox jCheckBox3;
-    private javax.swing.JCheckBox jCheckBox4;
-    private javax.swing.JCheckBox jCheckBox5;
-    private javax.swing.JCheckBox jCheckBox6;
-    private javax.swing.JCheckBox jCheckBox7;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JCheckBox recommendCollege1;
+    private javax.swing.JCheckBox recommendCollege2;
+    private javax.swing.JCheckBox recommendCollege3;
+    private javax.swing.JCheckBox recommendCollege4;
+    private javax.swing.JCheckBox recommendCollege5;
+    private javax.swing.JComboBox<String> streamFilter;
     // End of variables declaration//GEN-END:variables
 
 }
